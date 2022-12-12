f8a1d726e49621290e70fa9846dfe90d942c452f
samuelcleo <cleophassamuel@gmail.com>
2022-12-12 11:17:59 +1100

    Implemented Letter Checking
    - Created checkLetter function
    - Modified DOM Display letters for correctly guessed letters

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const phrases = [
     "Live and let die",
-    "Still waters run deep",
+    "Easy does it",
     "Alone in the crowd",
     "Dig deep",
-    "Cross that bridge when I come to it"
+    "Hang in there"
 ];
-let missed = 0;
 
 const getRandomPhrase = (array) => {
-    let randomIndex = Math.floor(Math.random() * (array.length + 1))
-    return [...array[randomIndex]]
+    let randomIndex = Math.floor(Math.random() * array.length);
+    let phrase = [ ...array[randomIndex] ]
+    return phrase
 };
-
-const start = document.getElementsByClassName('btn__reset')[0];
-const keyboard = document.getElementById('qwerty');
-const displayUl = document.getElementById('phrase');
 let phrase = getRandomPhrase(phrases);
 
+const start = document.getElementsByClassName('btn__reset')[0];
 start.addEventListener('click', () => {
     document.querySelector('.overlay');
     overlay.style.display = "none";
 });
+
+const keyboard = document.getElementById('qwerty');
+const displayUl = document.getElementById('phrase');
 
 for (let i = 0; i < phrase.length; i++) {
     if (phrase[i] !== " ") {
@@ -32,3 +32,19 @@ for (let i = 0; i < phrase.length; i++) {
     }
 }
 
+let missed = 0;
+function checkLetter (check) {
+    const letters = document.getElementsByClassName('letter');
+    for (let letter of letters) {
+        let eachLetter = letter.textContent.toLowerCase();
+        if (eachLetter === check) {
+            letter.className = "letter show";
+        }
+    }
+}
+keyboard.addEventListener('click', (event) => {
+    if ( event.target.tagName === "BUTTON") {
+        let letter = event.target.textContent;
+        checkLetter(letter);
+    }
+});
