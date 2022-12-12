9631461f1a4384fde445e883773e5b9723863f91
samuelcleo <cleophassamuel@gmail.com>
2022-12-12 12:50:48 +1100

    Implemented Win/Lose Functions
    - Added overlay changes
    - listening for score changes

--- app.js
+++ app.js
@@ -6,45 +6,82 @@ const phrases = [
     "Hang in there"
 ];
 
-const getRandomPhrase = (array) => {
+function getRandomPhrase(array) {
     let randomIndex = Math.floor(Math.random() * array.length);
     let phrase = [ ...array[randomIndex] ]
     return phrase
 };
-let phrase = getRandomPhrase(phrases);
 
+let phrase = getRandomPhrase(phrases);
+const overlay = document.getElementById('overlay'); 
 const start = document.getElementsByClassName('btn__reset')[0];
+// First hide the overlay
 start.addEventListener('click', () => {
-    document.querySelector('.overlay');
     overlay.style.display = "none";
 });
 
-const keyboard = document.getElementById('qwerty');
 const displayUl = document.getElementById('phrase');
+// Display all the letters of the phrase
+function displayPhrase() {
+    for (let i = 0; i < phrase.length; i++) {
+        if (phrase[i] !== " ") {
+            let letter = phrase[i];
+            let li = document.createElement('li');
+                li.textContent = letter;
+                li.className = "letter"
+            displayUl.appendChild(li);
+        }
+    }
+}
+displayPhrase();
 
-for (let i = 0; i < phrase.length; i++) {
-    if (phrase[i] !== " ") {
-        let letter = phrase[i];
-        let li = document.createElement('li');
-            li.textContent = letter;
-            li.className = "letter"
-        displayUl.appendChild(li);
+function checkWin() {
+    let correctLetters = document.getElementsByClassName('show');
+    
+    if (missed >= 5) {
+        overlay.removeAttribute('style');
+        overlay.className = "lose";
+        overlay.firstElementChild.textContent = "You Lose!";
+    } else if ( correctLetters.length === letters.length ) {
+        overlay.removeAttribute('style');
+        overlay.className = "win";
+        overlay.firstElementChild.textContent = "You Win!";
     }
 }
 
+// Check every hidden letter, show the matching ones, update the score, and take a heart away <3
+// Check if you've won
+const letters = document.getElementsByClassName('letter'); 
+const hearts = document.getElementsByClassName('tries');
 let missed = 0;
-function checkLetter (check) {
-    const letters = document.getElementsByClassName('letter');
+const checkLetter = (check) => {
+    let isCorrect = false;
+    // check every letter and show matching ones
     for (let letter of letters) {
         let eachLetter = letter.textContent.toLowerCase();
         if (eachLetter === check) {
             letter.className = "letter show";
+            isCorrect = true;
         }
     }
+    // update the score
+    if (!isCorrect) {
+        missed++;
+        //and take a heart away
+        hearts[missed - 1].firstElementChild.src="images/lostHeart.png";
+        
+        return null
+    } else {
+        return check;
+    }
 }
+
+const keyboard = document.getElementById('qwerty');
+// keyboard clicks run the checkLetter function
 keyboard.addEventListener('click', (event) => {
     if ( event.target.tagName === "BUTTON") {
         let letter = event.target.textContent;
         checkLetter(letter);
+        checkWin()
     }
 });
